1e2db1d1e69ab227c3cb4897dd0de6d0b1ccb6c1
sowndharya456 <sowndharyar12@gmail.com>
2020-03-29 13:08:46 +0530

    commit final changes

---
+++ scripts/postslist.js
@@ -0,0 +1,23 @@
+var trashModal=document.getElementById("trash-modal");
+var overlay=document.getElementById("overlay");
+function closeTrashWindow(){
+trashModal.style.display="none";
+}
+
+
+function deletePostWindow(postid){
+    trashModal.style.display="block";
+ // document.getElementById("overlay").style.display = "block";
+    window.onclick = function (event) {
+        if (event.target == trashModal) {
+            trashModal.style.display = "none";
+        }
+    };
+    const deletePostConfirm = document.getElementById('deletePostButton');
+    deletePostConfirm.onclick = function(){
+        postid.style.display="none";
+        trashModal.style.display = "none";
+//        document.getElementById("overlay").style.display = "none";
+    };
+}
+
